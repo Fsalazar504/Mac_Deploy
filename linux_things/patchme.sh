070a8b80db31fb46195c3deb9c5e11aaee12ba97
fsalazar504 <fsalazar504@gmail.com>
2023-04-28 23:35:45 -0700

    Add to repository, edit empty space

--- linux_things/patchme.sh
+++ linux_things/patchme.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-apt-get update
-sync-available
-apt-show-versions -u
-uname -r
-cat /etc/debian_version
-apt-get dist-upgrade --purge
+apt-get update &&
+sync-available &&
+apt-show-versions -u &&
+uname -r &&
+cat /etc/debian_version &&
+apt-get dist-upgrade --purge &&
 echo check what's getting updated and what's getting removed!
-dpkg -l | grep linux-image
+dpkg -l | grep linux-image &&
 rm_image= echo "What image do you want to remove?"
 read rm_image
 echo $rm_image
-apt-get remove --purge $rm_image
+apt-get remove --purge $rm_image &&
 reboot
-uname -r
-cat /etc/debian_version
-
